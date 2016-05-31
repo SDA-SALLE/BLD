d54b4c42a91e3d0ac6f097001803fd7e7c87d103
Cesar Augusto Palacios Alarcon <ceapalaciosal@gmail.com>
2016-05-31 11:18:35 -0500

    Creación BLD

---
+++ src/core/matriz.py
@@ -0,0 +1,42 @@
+# -*- encoding: utf-8 -*-
+
+#! /usr/bin/env python
+#created by @ceapalaciosal
+#under code Creative Commons
+
+#List Library Import
+import csv
+import os
+import xlrd
+import unicodedata
+import numpy as np
+
+
+
+def convertXLSX(direccion):
+
+	direccionexcel = direccion
+	workbook = xlrd.open_workbook(direccionexcel)
+	all_worksheets = workbook.sheet_names()
+
+	data = workbook.sheet_by_index(0) #Numero de Sheet donde se encuentran los datos / Sheet data 
+	direccioncsv = direccionexcel + '.csv'
+
+	data_emissions = open(''.join([direccioncsv]), 'wb') #crea el csv datos Base / Created CSV data Base
+	emissions = csv.writer(data_emissions, delimiter=',') #Abre el CSV para escritura de emsiones / Open CSV write emitions
+
+
+ 	for rownum in xrange(data.nrows):
+ 		emissions.writerow([entry for entry in data.row_values(rownum)])
+
+ 	
+ 	data_emissions.close()
+
+ 	matriz = convertCSV(direccioncsv)
+ 	return matriz
+
+
+def convertCSV(direccioncsv):
+
+	matriz = np.genfromtxt(direccioncsv, delimiter=',', dtype=None)
+	return  matriz
---
+++ src/core/register.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+#! /usr/bin/env python
+#created by @ceapalaciosal
+#under code Creative Commons
+
+#------------ Fuction list archives CSV in directory ---------#
+import os
+
+
+def listaCSV(direccion):
+   
+	path = os.path.join(direccion,'')
+	lstFilesEmissions = []
+	lstDir = os.walk(path)  
+
+	for root, dirs, files in lstDir:
+	    for fichero in files:
+	        (nombreFichero, extension) = os.path.splitext(fichero)
+	        if(extension == '.csv'):
+	        	lstFilesEmissions.append(nombreFichero+extension)
+
+	return lstFilesEmissions
